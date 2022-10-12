10d6facdfa87006e6297cb461674c8de52bb1c6e
Francisco Garcia <frangalo34@gmail.com>
2022-10-12 19:32:54 +0200

    Change resources view to the pages folder

---
+++ backoffice/src/views/Pages/Resources/ResourcesView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main_content">
+    <!-- <Sidebar /> -->
+    <div class="content">
+        Resources
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { authComputed } from "../../../store/helpers.js";
+import Sidebar from "../../../components/Sidebar.vue";
+export default {
+  name: "resourcesView",
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    
+  },
+  components: {
+    Sidebar,
+  },
+  methods: {
+        
+  },
+  computed: {
+    ...authComputed,
+  },
+};
+</script>;
