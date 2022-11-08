0ae620fe21d3095148ff3378db6d79606ec4fd41
Francisco Garcia <frangalo34@gmail.com>
2022-11-08 22:25:45 +0100

    edited template (added the Breadcrumb), new function to fetch the data.

--- backoffice/src/views/Pages/Questions/QuestionsView.vue
+++ backoffice/src/views/Pages/Questions/QuestionsView.vue
@@ -1,81 +1,89 @@
 <template>
   <div class="main_content">
-    <Sidebar />
-    <div class="content">
-        <div v-if="loggedIn" >
-        <div class="d-flex flex-row-reverse">
-          <router-link to="/addQuestion"><button  class="btn btn-success float-left btn-lg">Add question <i class="fa-solid fa-plus"></i></button></router-link>
+    <Breadcrumb />
+    <Sidebar>
+      <template v-slot:body>
+        <div class="content">
+          <div v-if="loggedIn">
+            <div class="d-flex flex-row-reverse">
+              <!-- <router-link to="/questionsDetailView"><button  class="btn btn-success float-left btn-lg">Add question <i class="bi bi-plus-square"></i></button></router-link> -->
+            </div>
+            <br />
+            <div class="table-wrapper-scroll-y my-custom-scrollbar">
+              <table class="table table-bordered table-striped table-hover mb-0">
+                <thead class="thead-dark">
+                  <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Text</th>
+                    <th scope="col">Answers</th>
+                    <th scope="col">Buttons</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(question, index) in questions" :key="question">
+                    <td>{{ question.id }}</td>
+                    <td>
+                      <span>
+                          {{ question.text.es }}
+                      </span>
+                    </td>
+                    <td>
+                      <span v-for="(answers, index2) in questions[index].answers" :key="answers">
+                        <ol>
+                          {{ questions[index].answers[index2].text.es }}
+                        </ol>
+                      </span>
+                    </td>
+                    <td>
+                      <span>
+                        <div class="btn-group">
+                          <router-link v-bind:to="`/paths`+`/${this.$route.params.path_id}`+`/tests/`+`${this.$route.params.test_id}`+`/questions/`+question.id">
+                            <button class="btn btn-outline-primary">
+                              Edit <i class="bi bi-list-ol"></i>
+                            </button>
+                          </router-link>
+                          <button class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+                        </div>
+                      </span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
-        <br>
-        <div class="table-wrapper-scroll-y my-custom-scrollbar">
-          <table class="table table-bordered table-striped table-hover mb-0">
-            <thead class="thead-dark">
-              <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Text</th>              
-                <th scope="col">Answers</th>
-                <th scope="col">Buttons</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(question ,index) in questions" :key="question">
-                <td>{{ question.id }}</td>
-                <td>
-                  <span>
-                    {{ question.text.es }}
-                  </span>
-                </td>
-                <td>
-                  <span v-for="(answers ,index2) in questions[index].answers" :key="answers">
-                    {{ questions[index].answers[index2].text.es }}
-                    <br>
-                  </span>
-                </td>
-                <td>
-                  <span v-if="updateForm && newId == path.id">
-                      <button class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i></button>
-                  </span>
-
-                  <span v-else>
-                    <button class="btn btn-secondary"><i class="fa-solid fa-pen-to-square"></i></button>
-                    <button class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
-                  </span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+      </template>
+    </Sidebar>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import { authComputed } from "../../../store/helpers.js";
-import Sidebar from "../../../components/Sidebar.vue";
+import { authComputed } from "@/store/helpers.js";
+import Sidebar from "@/components/Sidebar.vue";
+import Breadcrumb from "@/components/BreadCrumb.vue";
 export default {
   name: "questionsView",
   data() {
     return {
-      questions: null,
-      name: '',
+      ApiUrl: "http://localhost:3000",
+      questions: "",
     };
   },
   mounted() {
-    axios.get("http://localhost:3000/questions").then((response) => {
-      return this.questions = response.data
-    }).catch((error) => console.log(error));
+    this.fetchData();
   },
   components: {
     Sidebar,
+    Breadcrumb,
   },
   methods: {
-      get(){
-        axios.get("http://localhost:3000/questions").then((response) => {
-          return this.questions = response.data
-        }).catch((error) => console.log(error));
-      }
+    fetchData() {
+      axios
+        .get(this.ApiUrl+`/paths`+`/${this.$route.params.path_id}`+`/tests`+`/${this.$route.params.test_id}`+`/questions`)
+        .then((response) => {this.questions = response.data;})
+        .catch((error) => console.log(error));
+    },
   },
   computed: {
     ...authComputed,
