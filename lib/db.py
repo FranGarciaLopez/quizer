cd60fb8ce9d554561aa158bd8deb33b71cc85ce0
Francisco Garcia <frangalo34@gmail.com>
2022-12-06 20:40:45 +0100

    edited response from two functions in db.py from the api

--- lib/db.py
+++ lib/db.py
@@ -9,19 +9,19 @@ class Db:
         sql_statement = sql_statement.format(table_name, id_name, id_value)
 
         response = self.conn.engine.execute(sql_statement)
-        return Response_Parser.delete(response)
+        return Response_Parser.get(response)
 
     def get_subelement(self, table_name, id_name, id_value, reference_name, reference_id):
         sql_statement = "SELECT * FROM {0} WHERE '{1}' = '{2}' and '{3}' = '{4}'"
         sql_statement = sql_statement.format(table_name, id_name, id_value, reference_name, reference_id)
 
         response = self.db.engine.execute(sql_statement)
-        return Response_Parser.delete(response)
+        return Response_Parser.get(response)
 
     def delete_all_subelement(self, table_name, id_name, id_value):
         sql_statement = "DELETE FROM {0} WHERE '{1}' = '{2}'"
         sql_statement = sql_statement.format(table_name, id_name, id_value)
-
+        
         response = self.conn.engine.execute(sql_statement)
         return Response_Parser.delete(response)
 
