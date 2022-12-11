37c472af5fe134ef500110dd5cb6f828f9f50740
Francisco Garcia <frangalo34@gmail.com>
2022-12-11 20:12:07 +0100

    Add install requirements before runing the test

---
+++ backoffice/scripts/run_bg.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+
+echo "Starting API"
+cd "../../api" 
+mkdir -p ./logs
+echo "  - Enabling virtual environment" && source "venv/bin/activate" > ./logs/api.log 
+echo "  - Installing dependencies" && pip install -r requirements.txt >> ./logs/api.log 
+echo "  - Starting server" && python3 app.py >> ./logs/api.log &
+echo "  - Server running"
+sleep 3
+
+echo ""
+echo "Starting Backoffice"
+cd "../backoffice"
+mkdir -p ./logs
+#echo "  - Installing dependencies" && npm install > ./logs/backoffice.log 
+echo "  - Starting service" npm run serve >> ./logs/backoffice.log &
+sleep 10
+
+echo "Stopping services"
+
+kill $(ps | grep npm | awk {'print$1'})
+kill $(ps | grep node | awk {'print$1'})
+echo "  - Backoffice stopped"
+kill $(ps | grep python3 | awk {'print$1'})
+echo "  - API stopped"
