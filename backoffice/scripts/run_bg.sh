7dd58e803a2ba2773d335217ea9938deb0dcaaef
Francisco Garcia <frangalo34@gmail.com>
2022-12-20 20:20:03 +0100

    change directory services script

--- backoffice/scripts/run_bg.sh
+++ backoffice/scripts/run_bg.sh
@@ -3,19 +3,19 @@
 echo "Starting API"
 cd "../../api"
 mkdir -p ./logs
-echo "  - Enabling virtual environment" && source "venv/bin/activate" > ./logs/api.log
-echo "  - Installing dependencies" && pip install -r requirements.txt  >> ./logs/api.log
-echo "  - Starting server" && python3 app.py >> ./logs/api.log &
+echo "  - Enabling virtual environment" && source "venv/bin/activate" > ./api/logs/api.log
+echo "  - Installing dependencies" && pip install -r requirements.txt  >> ./api/logs/api.log
+echo "  - Starting server" && python3 app.py >> ./api/logs/api.log &
 echo "  - Server running"
 sleep 10
 
 echo ""
 echo "Starting Backoffice"
 cd "../backoffice"
-mkdir -p ./logs
-echo "  - Installing vue-cli-service" && npm install -g @vue/cli-service > ./logs/backoffice.log
-echo "  - Installing dependencies" && npm install --legacy-peer-deps >> ./logs/backoffice.log
-echo "  - Starting service" && npm run serve >> ./logs/backoffice.log &
+mkdir -p ./backoffice/logs
+echo "  - Installing vue-cli-service" && npm install -g @vue/cli-service > ./backoffice/logs/backoffice.log
+echo "  - Installing dependencies" && npm install --legacy-peer-deps >> ./backoffice/logs/backoffice.log
+echo "  - Starting service" && npm run serve >> ./backoffice/logs/backoffice.log &
 sleep 10
 
 
