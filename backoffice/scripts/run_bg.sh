1cf54eb1153e4fba1e7b2a0387f26fbdc30d19ca
Francisco Garcia <frangalo34@gmail.com>
2022-12-20 18:47:22 +0100

    test if the script runs propperly

--- backoffice/scripts/run_bg.sh
+++ backoffice/scripts/run_bg.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
-#pg_dump -U postgres tfg-db > /backup/tfg-db.sql
-#psql -U postgres -f /backup/tfg-db.sql
+
 echo "Starting API"
 cd "../../api"
-mkdir "./logs" 
-echo "  - Enabling virtual environment" && source "venv/bin/activate" > ./logs/api_logs.log &
-echo "  - Installing dependencies" && pip install -r requirements.txt  >> ./logs/api_logs.log &
-echo "  - Starting server" && python3 app.py >> ./logs/api_logs.log &
+mkdir -p ./logs
+echo "  - Enabling virtual environment" && source "venv/bin/activate" > ./logs/api.log
+echo "  - Installing dependencies" && pip install -r requirements.txt  >> ./logs/api.log
+echo "  - Starting server" && python3 app.py >> ./logs/api.log &
 echo "  - Server running"
 sleep 10
 
 echo ""
 echo "Starting Backoffice"
 cd "../backoffice"
-mkdir "./logs" 
-echo "  - Installing vue-cli-service" && npm install -g @vue/cli-service > ./logs/backoffice.log &
-echo "  - Installing dependencies" && npm install --legacy-peer-deps >> ./logs/backoffice.log &
+mkdir -p ./logs
+echo "  - Installing vue-cli-service" && npm install -g @vue/cli-service > ./logs/backoffice.log
+echo "  - Installing dependencies" && npm install --legacy-peer-deps >> ./logs/backoffice.log
 echo "  - Starting service" && npm run serve >> ./logs/backoffice.log &
 sleep 10
 
