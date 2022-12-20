d64eacc529cfdc5cfd8c16ddf14c4f8841d1fe07
Francisco Garcia <frangalo34@gmail.com>
2022-12-20 13:10:13 +0100

    added psycopg2 to run_bg.sh script

--- backoffice/scripts/run_bg.sh
+++ backoffice/scripts/run_bg.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
-
+#pg_dump -U postgres tfg-db > /backup/tfg-db.sql
+#psql -U postgres -f /backup/tfg-db.sql
 echo "Starting API"
 cd "../../api" 
-mkdir -p ./logs
-echo "  - Enabling virtual environment" && source "venv/bin/activate" > ./logs/api.log 
+
+echo "  - Enabling virtual environment" && source "venv/bin/activate" >> ./logs/api.log &
+pip install psycopg2 &
 echo "  - Installing dependencies" && pip install -r requirements.txt >> ./logs/api.log 
 echo "  - Starting server" && python3 app.py >> ./logs/api.log &
 echo "  - Server running"
@@ -14,10 +16,16 @@ sleep 3
 echo ""
 echo "Starting Backoffice"
 cd "../backoffice"
-mkdir -p ./logs
-#echo "  - Installing dependencies" && npm install > ./logs/backoffice.log 
-echo "  - Starting service" npm run serve >> ./logs/backoffice.log &
+
+echo "  - Installing dependencies" && npm install --legacy-peer-deps >> ./logs/backoffice.log
+echo "  - Starting service" && npm run serve >> ./logs/backoffice.log &
 sleep 10
+
+echo ""
+echo "Starting Tests"
+cd "../backoffice"
+echo "  - Starting e2e tests" && npx nightwatch tests/tests.e2e.test.js --reuse-browser >> ./logs/backoffice.log &
+sleep 5
 
 echo "Stopping services"
 
