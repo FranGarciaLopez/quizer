e66716c757a9bda908c777bb03c0c08ca303fcc0
Francisco Garcia <frangalo34@gmail.com>
2022-12-20 14:08:57 +0100

    installing vue-cli-service to the script that runs the api and backoffice on vue

--- backoffice/scripts/run_bg.sh
+++ backoffice/scripts/run_bg.sh
@@ -3,7 +3,7 @@
 #psql -U postgres -f /backup/tfg-db.sql
 echo "Starting API"
 cd "../../api"
-mkdir ./logs &
+mkdir ./logs/api_logs.log &
 echo "  - Enabling virtual environment" && source "venv/bin/activate" > ./logs/api_logs.log &
 echo "  - Installing dependencies" && pip install -r requirements.txt  >> ./logs/api_logs.log &
 echo "  - Starting server" && python3 app.py >> ./logs/api_logs.log &
@@ -13,9 +13,9 @@ sleep 10
 echo ""
 echo "Starting Backoffice"
 cd "../backoffice"
-mkdir ./logs &
-
-echo "  - Installing dependencies" && npm install --legacy-peer-deps > ./logs/backoffice.log &
+mkdir ./logs/backoffice.log &
+echo "  - Installing vue-cli-service" && npm install -g @vue/cli-service > ./logs/backoffice.log &
+echo "  - Installing dependencies" && npm install --legacy-peer-deps >> ./logs/backoffice.log &
 echo "  - Starting service" && npm run serve >> ./logs/backoffice.log &
 sleep 10
 
