632182b1bd52c505aeb4ccd181db0738c744e5e4
Francisco Garcia <frangalo34@gmail.com>
2023-01-02 11:59:14 +0100

    edited CardView component to show the result of a test when the tests is made or display a message ' You have not done the test yet.'

--- backoffice/src/components/CardView.vue
+++ backoffice/src/components/CardView.vue
@@ -10,15 +10,22 @@
                         <hr>
                         <footer class="footer">
                             <slot name="link-button">
-                                <router-link :to="this.link+`/${data.id}`">
-                                    <button class="btn btn-primary btn-lg mr-2">{{this.buttonText}}</button>
-                                </router-link>
-                            </slot>
-                            <slot name="completed-icon">
-                                <button class="btn btn-secondary btn-lg mr-2">
-                                    <i class="bi bi-check-circle"  v-if="data.satatus == 'active'"></i>
-                                    <i class="bi bi-check-circle-fill"  v-else></i>
-                                </button>
+                                    <div  class="float-right">
+                                        <router-link :to="this.link+`/${data.id}`" v-if="(this.$route.name === 'userTestsView')">
+                                            <button class="btn btn-primary btn-lg mr-2" @click="this.insertUserTest(data.id)">{{this.buttonText}}</button>
+                                        </router-link>
+                                        <router-link :to="this.link+`/${data.id}`" v-else>
+                                            <button class="btn btn-primary btn-lg mr-2">{{this.buttonText}}</button>
+                                        </router-link>
+                                    </div>
+                                    <div class="float-left">
+                                        <h3 v-if="data.acc_results && (Object.keys(data.acc_results).length !== 0) && (this.$route.name === 'userTestsView')">
+                                            Score: {{data.acc_results.right}}/{{data.acc_results.total}}
+                                        </h3>
+                                        <h3 v-else-if="this.$route.name === 'userTestsView'">
+                                            You have not done the test yet.
+                                        </h3>
+                                    </div>
                             </slot>
                         </footer>
                     </section>
@@ -30,8 +37,15 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: 'cardView',
+    data() {
+        return {
+            ApiUrl: "http://localhost:3000",
+        };
+    },
     props: {
         text: {
             type: Object,
@@ -42,8 +56,12 @@ export default {
         link: {
             type: String,        
         }
-
     },
-
+    methods: {
+        insertUserTest(testId) {
+            axios.post(this.ApiUrl + `/users/${this.$route.params.user_id}/tests/${testId}`);
+            
+        },
+    }
 }
 </script>
