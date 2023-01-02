3e99dec49afba2d249cf5c3aa7cf028ea33ff08f
Francisco Garcia <frangalo34@gmail.com>
2023-01-02 17:48:49 +0100

    changed folder to run tests in nightwatch on the CI file

--- backoffice/nightwatch.conf.js
+++ backoffice/nightwatch.conf.js
@@ -14,7 +14,7 @@
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['tests','nightwatch/examples'],
+  src_folders: ['tests','tests/tests.e2e.test.js'],
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
   page_objects_path: ['nightwatch/page-objects'],
