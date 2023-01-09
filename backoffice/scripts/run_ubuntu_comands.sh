d6532db477f66917591c49d96b7d67c9f4793d5d
Francisco Garcia <frangalo34@gmail.com>
2023-01-09 10:21:30 +0100

    commands for postgres backup updated (create database backup)

--- backoffice/scripts/run_ubuntu_comands.sh
+++ backoffice/scripts/run_ubuntu_comands.sh
@@ -7,10 +7,12 @@ echo "Installing postgres on Ubuntu"
 cd "../../api" 
 mkdir -p ./logs
 echo "  - Installing postgres..." &&  sudo apt-get update > ./logs/postgres_logs.log 
-echo "  - Installing postgres..." &&  sudo apt install postgresql postgresql-contrib >> ./logs/postgres_logs.log 
-echo "  - Updating postgres service" && sudo service postgresql start >> ./logs/postgres_logs.log 
-echo "  - Accessing postgres" && sudo -u postgres psql >> ./logs/postgres_logs.log 
-echo "  - Creating database backup for tests" && pg_dump -U postgres tfg-db > ./backup/tfg-db.sql >> ./logs/postgres_logs.log 
+echo "  - Installing postgres..." &&  sudo apt install postgresql postgresql--client >> ./logs/postgres_logs.log 
+echo "  - Updating postgres service" && sudo -u postgres psql >> ./logs/postgres_logs.log 
+echo "  - Accessing postgres" && sudo -u postgres psql >> ./logs/postgres_logs.log
+echo "  - Creating new user" && sudo -u postgres createuser --interactive >> ./logs/postgres_logs.log
+echo "  - Creating new database" && sudo -u postgres createdb tfg-db >> ./logs/postgres_logs.log
+echo "  - Creating database backup for tests" && pg_dump -U postgres -d backup -f ./backup/tfg-db.sql >> ./logs/postgres_logs.log 
 sleep 10
 
 echo "Installing headless chromium"
@@ -20,10 +22,3 @@ echo "  - Installing dependencies..." &&  sudo apt-get install -y libappindicato
 echo "  - Installing dependencies..." &&  sudo apt-get install -f >> ./logs/chromium_logs.log
 echo "  - Downloading headless Chromium" && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb >> ./logs/chromium_logs.log
 echo "  - Installing headless Chromium" && sudo dpkg -i google-chrome*.deb >> ./logs/chromium_logs.log
-
-
-#kill $(ps | grep npm | awk {'print$1'})
-#kill $(ps | grep node | awk {'print$1'})
-#echo "  - Backoffice stopped"
-#kill $(ps | grep python3 | awk {'print$1'})
-#echo "  - API stopped"
