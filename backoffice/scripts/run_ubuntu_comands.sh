6473858908335a7df153c7f9f30c6a7f126a4a28
Francisco Garcia <frangalo34@gmail.com>
2023-01-09 18:13:31 +0100

    ci updating postgres commands

--- backoffice/scripts/run_ubuntu_comands.sh
+++ backoffice/scripts/run_ubuntu_comands.sh
@@ -4,6 +4,16 @@
 #pg_dump -U postgres tfg-db > /backup/tfg-db.sql
 #psql -U postgres -f /backup/tfg-db.sql
 
+echo "Installing postgres on Ubuntu"
+cd "../../api/backup/"
+mkdir -p ./logs
+echo "  - Installing postgres..."             && sudo apt-get update                                             > ./logs/postgres_logs.log
+echo "  - Installing postgres..."             && sudo apt install postgresql postgresql-client                  >> ./logs/postgres_logs.log
+echo "  - Starting postgres server"           && sudo -u postgres pg_ctlcluster 12 main start                   >> ./logs/postgres_logs.log
+echo "  - Creating new database"              && sudo -u postgres createdb tfg-db                               >> ./logs/postgres_logs.log
+echo "  - Creating database backup for tests" && pg_dump -U postgres -d tfg-db > tfg-db.sql                     >> ./logs/postgres_logs.log
+sleep 10
+
 echo "Installing headless chromium"
 mkdir -p ./logs
 cd "../backoffice"
