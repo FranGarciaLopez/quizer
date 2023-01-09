8a0c825bcf08da5ccffdf9519f1e90f56afbab6f
Francisco Garcia <frangalo34@gmail.com>
2023-01-09 20:03:23 +0100

    single backlash in the command

--- backoffice/scripts/run_ubuntu_comands.sh
+++ backoffice/scripts/run_ubuntu_comands.sh
@@ -7,13 +7,11 @@
 echo "Installing postgres on Ubuntu"
 cd "../../api/backup/"
 mkdir -p ./logs
-echo "  - Installing postgres"             && sudo apt-get update && sudo apt-get install -q postgresql-client                  >> ./logs/postgres_logs.log
+echo "  - Installing postgres"             && sudo apt-get update && sudo apt-get install -qq postgresql-client                  >> ./logs/postgres_logs.log
 echo "  - Loading database backup for tests" && psql postgresql://postgres:changeme@localhost:5432 -f tfg-db.sql           >> ./logs/postgres_logs.log
-psql postgresql://postgres:changeme@localhost:5432 \\dt
+psql postgresql://postgres:changeme@localhost:5432 \dt
 
 echo "Installing headless chromium"
-mkdir -p ./logs
-cd "../backoffice"
 mkdir -p ./logs
 echo "  - Installing dependencies..." &&  sudo apt-get install -y libappindicator1 fonts-liberation > ./logs/chromium_logs.log
 #echo "  - Installing dependencies..." &&  sudo apt-get install -f >> ./logs/chromium_logs.log
