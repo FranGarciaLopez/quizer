2d43518d267322c206cea522c03a4621f2d059bb
Francisco Garcia <frangalo34@gmail.com>
2023-01-09 19:48:42 +0100

    donwload chromiun un silent mode

--- backoffice/scripts/run_ubuntu_comands.sh
+++ backoffice/scripts/run_ubuntu_comands.sh
@@ -7,12 +7,8 @@
 echo "Installing postgres on Ubuntu"
 cd "../../api/backup/"
 mkdir -p ./logs
-echo "  - Installing postgres..."             && sudo apt-get update                                             > ./logs/postgres_logs.log
-echo "  - Installing postgres..."             && sudo apt install postgresql-client                  >> ./logs/postgres_logs.log
-#echo "  - Starting postgres server"           && sudo -u postgres pg_ctlcluster 12 main start                   >> ./logs/postgres_logs.log
-#echo "  - Creating new database"              && sudo -u postgres createdb tfg-db                               >> ./logs/postgres_logs.log
-pwd
-echo "  - Creating database backup for tests" && psql postgresql://postgres:changeme@localhost:5432 -f tfg-db.sql                     >> ./logs/postgres_logs.log
+echo "  - Installing postgres..."             && sudo apt-get update && sudo apt-get install postgresql-client                  >> ./logs/postgres_logs.log
+echo "  - Creating database backup for tests" && psql postgresql://postgres:changeme@localhost:5432 -f tfg-db.sql           >> ./logs/postgres_logs.log
 
 
 echo "Installing headless chromium"
@@ -21,5 +17,5 @@ cd "../backoffice"
 mkdir -p ./logs
 echo "  - Installing dependencies..." &&  sudo apt-get install -y libappindicator1 fonts-liberation > ./logs/chromium_logs.log
 echo "  - Installing dependencies..." &&  sudo apt-get install -f >> ./logs/chromium_logs.log
-echo "  - Downloading headless Chromium" && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb >> ./logs/chromium_logs.log
+echo "  - Downloading headless Chromium" && wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb >> ./logs/chromium_logs.log
 echo "  - Installing headless Chromium" && sudo dpkg -i google-chrome*.deb >> ./logs/chromium_logs.log
