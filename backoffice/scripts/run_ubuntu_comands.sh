0f507a6cec3c4ae3672a95fc5748a976e98739a9
Francisco Garcia <frangalo34@gmail.com>
2023-01-10 11:15:40 +0100

    added --clean to the pg_restore + inserting new script

--- backoffice/scripts/run_ubuntu_comands.sh
+++ backoffice/scripts/run_ubuntu_comands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#pg_restore -U postgres -Ft -v -d tfg-db tfg-db.sql
-#pg_dump -U postgres tfg-db > /backup/tfg-db.sql
+#pg_restore -U postgres -Ft -v -C -d --clean tfg-db /backup/tfg-db.sql
+#pg_dump -U postgres -Ft -v --data-only tfg-db > /backup/tfg-db.sql
 #psql -U postgres -f /backup/tfg-db.sql
 
 echo "Installing postgres on Ubuntu"
@@ -10,7 +10,7 @@ mkdir -p ./logs
 echo "  - Installing postgres"    && sudo apt-get update && sudo apt-get install -qq postgresql-client                >> ./logs/postgres_logs.log
 echo "  - Create database"        && psql postgresql://postgres:changeme@localhost:5432 -c "CREATE DATABASE \"tfg-db\"" >> ./logs/postgres_logs.log
 #echo "  - Loading data for tests" && psql postgresql://postgres:changeme@localhost:5432 -f tfg-db.sql                 >> ./logs/postgres_logs.log
-echo "  - Loading data for tests" && export PGPASSWORD=yourpassword && pg_restore -h localhost -p 5434 -U postgres -Ft -v -C -d tfg-db tfg-db.sql
+echo "  - Loading data for tests" && export PGPASSWORD=yourpassword && pg_restore -U postgres -Ft -v -C -d --clean tfg-db /backup/tfg-db.sql
 psql postgresql://postgres:changeme@localhost:5432 \dt
 
 echo "Installing headless chromium"
