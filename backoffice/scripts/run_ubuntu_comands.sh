2368e02f750ff52b390473555a7f76bad63864c5
Francisco Garcia <frangalo34@gmail.com>
2023-01-11 14:00:12 +0100

    Revert "Installing google-chrome version 107 with the bash script"
    This reverts commit 81be453ca0c680e24bee07a78c5c47b2440d58ab.
    
    # Conflicts:
    #	backoffice/scripts/run_ubuntu_comands.sh

--- backoffice/scripts/run_ubuntu_comands.sh
+++ backoffice/scripts/run_ubuntu_comands.sh
@@ -9,6 +9,6 @@ mkdir -p ./logs
 cd "../backoffice"
 mkdir -p ./logs
 echo "  - Installing dependencies..." &&  sudo apt-get install -y libappindicator1 fonts-liberation > ./logs/chromium_logs.log
-echo "  - Installing dependencies..." &&  sudo apt-get install -f >> ./logs/chromium_logs.log
-echo "  - Downloading headless Chromium" && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb >> ./logs/chromium_logs.log
+#echo "  - Installing dependencies..." &&  sudo apt-get install -f >> ./logs/chromium_logs.log
+echo "  - Downloading headless Chromium" && wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb >> ./logs/chromium_logs.log
 echo "  - Installing headless Chromium" && sudo dpkg -i google-chrome*.deb >> ./logs/chromium_logs.log
