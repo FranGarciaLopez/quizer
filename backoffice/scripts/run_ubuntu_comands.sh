81be453ca0c680e24bee07a78c5c47b2440d58ab
Francisco Garcia <frangalo34@gmail.com>
2023-01-11 13:53:38 +0100

    Installing google-chrome version 107 with the bash script

--- backoffice/scripts/run_ubuntu_comands.sh
+++ backoffice/scripts/run_ubuntu_comands.sh
@@ -15,5 +15,5 @@ echo "Installing headless chromium"
 mkdir -p ./logs
 echo "  - Installing dependencies..." &&  sudo apt-get install -y libappindicator1 fonts-liberation > ./logs/chromium_logs.log
 #echo "  - Installing dependencies..." &&  sudo apt-get install -f >> ./logs/chromium_logs.log
-echo "  - Downloading headless Chromium" && wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb >> ./logs/chromium_logs.log
+echo "  - Downloading headless Chromium" && wget -q https://dl.google.com/linux/direct/google-chrome-stable_107.0.0-1_amd64.deb >> ./logs/chromium_logs.log
 echo "  - Installing headless Chromium" && sudo dpkg -i google-chrome*.deb >> ./logs/chromium_logs.log
