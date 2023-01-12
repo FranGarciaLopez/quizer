6553cbf2669d1af662a90de944182c595235bfc7
Francisco Garcia <frangalo34@gmail.com>
2023-01-12 18:52:34 +0100

    added '/admin' to the routes on waitElement

--- backoffice/tests/questions_views.e2e.test.js
+++ backoffice/tests/questions_views.e2e.test.js
@@ -11,13 +11,13 @@ describe('Tests', function() {
         browser
             .assert.urlEquals('http://localhost:3001/admin/paths/2/tests/20/questions')
             .click('.float-right')
-            .assert.urlEquals('http://localhost:3001/paths/2/tests/20/questions/new')
+            .assert.urlEquals('http://localhost:3001/admin/paths/2/tests/20/questions/new')
             .pause(1000)
     });
 
     it(when+' if the user is at "/paths/2/tests/20/questions/new" URL and wants to add a question to the test with id "20"', function(browser) {
         browser
-            .assert.urlEquals('http://localhost:3001/paths/2/tests/20/questions/new')
+            .assert.urlEquals('http://localhost:3001/admin/paths/2/tests/20/questions/new')
 
             .waitForElementPresent('.mb-4 > form #formText')
 
@@ -32,11 +32,11 @@ describe('Tests', function() {
 
             .click('.card-body .btn-save')
             .waitForElementPresent('.table-responsive', 1000)
-            .assert.urlEquals('http://localhost:3001/paths/2/tests/20/questions')
+            .assert.urlEquals('http://localhost:3001/admin/paths/2/tests/20/questions')
             .pause(2000)
     });
  
-    it(when+' if the user has created a new question from a test and wants to edit it then he must go to "/paths/id/tests/id/question/id" URL and edit a field from the form then he should be redirected to "/paths/id/tests/id/questions" with the edited data', function(browser) {
+    it(when+' if the user has created a new question from a test and wants to edit it then he must go to "/admin/paths/id/tests/id/question/id" URL and edit a field from the form then he should be redirected to "/paths/id/tests/id/questions" with the edited data', function(browser) {
         browser
             .click('tr:nth-child(5) a > .btn')
             .waitForElementPresent('.mb-4 > form #formText')
@@ -54,15 +54,15 @@ describe('Tests', function() {
             .click('a[href="/admin/paths/2/tests/20/questions"]')
             .pause(1000)
 
-            .assert.urlEquals('http://localhost:3001/paths/2/tests/20/questions')
+            .assert.urlEquals('http://localhost:3001/admin/paths/2/tests/20/questions')
     });
  
     it(when+' if the user wants to delete a question from a path and clicks the delete button then it should be deleted', function(browser) {
         browser
-            .url('http://localhost:3001/paths/2/tests/20/questions')
+            .url('http://localhost:3001/admin/paths/2/tests/20/questions')
             .waitForElementPresent('.table-responsive')
 
             .click(`tr:nth-child(5) .btn-group > .btn > .bi`)
-            .assert.urlEquals('http://localhost:3001/paths/2/tests/20/questions')
+            .assert.urlEquals('http://localhost:3001/admin/paths/2/tests/20/questions')
     });
 });
