52bca62e8aca78b5864cfee2da01ee774dc78dcd
Francisco Garcia <frangalo34@gmail.com>
2023-01-13 14:23:56 +0100

    comenting questions from general test file.

--- backoffice/tests/tests.e2e.test.js
+++ backoffice/tests/tests.e2e.test.js
@@ -2,5 +2,5 @@ const login = require('./login_view.e2e.test');
 const users = require('./users_views.e2e.test');
 const paths = require('./paths_views.e2e.test');
 const tests = require('./tests_views.e2e.test');
-const questions = require('./questions_views.e2e.test');
+// const questions = require('./questions_views.e2e.test');
 
