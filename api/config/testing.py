a4e631e4214efdcf916782a5aef2a39af0970c37
Francisco Garcia <frangalo34@gmail.com>
2023-01-23 11:08:06 +0100

    changed docker-compose directory. added 2 Dockerfile for creating dockerization in the API and the web and changed ports to read the ones on docker.

--- api/config/testing.py
+++ api/config/testing.py
@@ -1,9 +1,9 @@
 class TestingConfig():
     TESTING = True
-    DEBUG = False
+    DEBUG = True
     ENV = 'testing'
 
     JWT_SECRET_KEY = "5iNySaf97&NW"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:changeme@localhost:5432/tfg-db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:changeme@host.docker.internal:5432/tfg-db"
