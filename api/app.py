482d1ef5c8382d464c72137805e3b7c5df389775
Francisco Garcia <frangalo34@gmail.com>
2023-01-23 13:06:25 +0100

    changed docke-compose directory. updated packages from web for avoid incompatibilities and created dockerfile for dockerizing apps

--- api/app.py
+++ api/app.py
@@ -7,20 +7,19 @@ from config.config import load_config
 
 app = Flask(__name__)
 
-config = load_config("production")
+config = load_config("testing")
 
 app.config["JWT_SECRET_KEY"] = config.JWT_SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = config.SQLALCHEMY_TRACK_MODIFICATIONS
 app.config["ENV"] = config.ENV
 
+from routes.routes import *
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
 CORS(app)
-
-
-from routes.routes import *
 
 if __name__ == "__main__":
     app.config.from_object(config)
