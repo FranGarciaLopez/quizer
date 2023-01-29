8d5ee3ff87a4a1cbc351658ecde4b2586d8180a7
Francisco Garcia <frangalo34@gmail.com>
2023-01-29 09:48:40 +0100

    Deleting registerView from the web.

--- backoffice/src/router/auth/index.js
+++ backoffice/src/router/auth/index.js
@@ -1,5 +1,4 @@
 import Login from '@/views/Pages/Auth/LoginView.vue'
-import Register from '@/views/Pages/Auth/RegisterView.vue'
 import PageNotFound from '@/components/PageNotFound.vue'
 
 let routes = [
@@ -7,11 +6,6 @@ let routes = [
         path: '/login',
         name: 'login',
         component: Login,
-    },
-    {
-        path: '/register',
-        name: 'register',
-        component: Register,
     },
     {
         path: '/:pathMatch(.*)*',
