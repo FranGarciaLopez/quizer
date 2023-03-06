b448d4758c3624e0a75084c1e562e4ecabca7096
Fran <frangalo34@gmail.com>
2023-03-06 19:18:07 +0100

    update conection string on testing file

--- api/config/testing.py
+++ api/config/testing.py
@@ -7,8 +7,8 @@ class TestingConfig():
     JWT_SECRET_KEY = "5iNySaf97&NW"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+
     if os.environ.get('GITHUB_ACTIONS'):
-        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:changeme@localhost:5432/tfg-db"
+        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:changeme@localhost:5432/tfg-db"
     else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:changeme@host.docker.internal:5432/tfg-db"
+        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:changeme@host.docker.internal:5432/tfg-db"
