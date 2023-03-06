6845fc5e5555b7c53af433d46577bb7e4f79b908
Fran <frangalo34@gmail.com>
2023-03-06 19:14:07 +0100

    solve github actions problem with tests?

--- api/config/testing.py
+++ api/config/testing.py
@@ -1,3 +1,4 @@
+import os
 class TestingConfig():
     TESTING = True
     DEBUG = True
@@ -6,4 +7,8 @@ class TestingConfig():
     JWT_SECRET_KEY = "5iNySaf97&NW"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:changeme@host.docker.internal:5432/tfg-db"
+    
+    if os.environ.get('GITHUB_ACTIONS'):
+        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:changeme@localhost:5432/tfg-db"
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:changeme@host.docker.internal:5432/tfg-db"
